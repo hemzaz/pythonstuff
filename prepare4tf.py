7194eac697a8b87a9f77f71853192a85c0bbb428
Ofer Chen <oferchen@gmail.com>
2024-01-03 13:24:20 +0100

    Update prepare4tf.py

--- prepare4tf.py
+++ prepare4tf.py
@@ -1,91 +1,87 @@
 #!/usr/bin/env python3
 import boto3
-import os
-import sys
+import argparse
 from pathlib import Path
-
-# Retrieve the environment name from the command line arguments or use the current directory name
-if len(sys.argv) > 2:
-    print("Usage: python create_terraform_backend.py [envname]")
-    sys.exit(1)
-
-envname = sys.argv[1] if len(sys.argv) == 2 else Path.cwd().name
-account_name = "company"  # Replace with your actual company name
-bucket_name = f"{account_name}-{envname}-tf-backend"
-dynamodb_table_name = "terraform-state-lock-dynamo"
-
-# Fetch the AWS default region from environment variable
-region = os.environ.get("AWS_DEFAULT_REGION")
-if not region:
-    print("AWS_DEFAULT_REGION environment variable is not set.")
-    sys.exit(1)
-
-# Initialize the S3 and DynamoDB clients
-s3_client = boto3.client("s3", region_name=region)
-dynamodb_client = boto3.client("dynamodb", region_name=region)
+from botocore.exceptions import ClientError
 
 
-# Create an S3 bucket for Terraform state storage
-def create_s3_bucket(bucket_name):
-    try:
-        if region == "us-east-1":
-            s3_client.create_bucket(Bucket=bucket_name)
-            print(f"S3 bucket '{bucket_name}' created successfully.")
-        else:
-            s3_client.create_bucket(
-                Bucket=bucket_name,
-                CreateBucketConfiguration={"LocationConstraint": region},
+class TerraformBackendCreator:
+    def __init__(self, envname, region, account_name="company"):
+        self.envname = envname
+        self.region = region
+        self.account_name = account_name
+        self.bucket_name = f"{self.account_name}-{self.envname}-tf-backend"
+        self.dynamodb_table_name = "terraform-state-lock-dynamo"
+        self.s3_client = boto3.client("s3", region_name=self.region)
+        self.dynamodb_client = boto3.client("dynamodb", region_name=self.region)
+
+    def create_s3_bucket(self):
+        try:
+            if self.region == "us-east-1":
+                self.s3_client.create_bucket(Bucket=self.bucket_name)
+            else:
+                self.s3_client.create_bucket(
+                    Bucket=self.bucket_name,
+                    CreateBucketConfiguration={"LocationConstraint": self.region},
+                )
+            print(f"S3 bucket '{self.bucket_name}' created successfully.")
+        except ClientError as e:
+            print(f"Failed to create S3 bucket: {e}")
+
+    def enable_bucket_versioning(self):
+        try:
+            self.s3_client.put_bucket_versioning(
+                Bucket=self.bucket_name, VersioningConfiguration={"Status": "Enabled"}
             )
-        print(f"S3 bucket '{bucket_name}' created successfully.")
-    except Exception as e:
-        print(f"Failed to create S3 bucket: {e}")
+            print(f"Versioning enabled on S3 bucket '{self.bucket_name}'.")
+        except ClientError as e:
+            print(f"Failed to enable versioning: {e}")
 
+    def create_dynamodb_table(self):
+        try:
+            self.dynamodb_client.create_table(
+                TableName=self.dynamodb_table_name,
+                AttributeDefinitions=[{"AttributeName": "LockID", "AttributeType": "S"}],
+                KeySchema=[{"AttributeName": "LockID", "KeyType": "HASH"}],
+                BillingMode="PAY_PER_REQUEST",
+            )
+            print(f"DynamoDB table '{self.dynamodb_table_name}' created successfully.")
+        except ClientError as e:
+            print(f"Failed to create DynamoDB table: {e}")
 
-# Enable versioning on the S3 bucket
-def enable_bucket_versioning(bucket_name):
-    try:
-        s3_client.put_bucket_versioning(
-            Bucket=bucket_name, VersioningConfiguration={"Status": "Enabled"}
-        )
-        print(f"Versioning enabled on S3 bucket '{bucket_name}'.")
-    except Exception as e:
-        print(f"Failed to enable versioning: {e}")
-
-
-# Create a DynamoDB table for Terraform state locking
-def create_dynamodb_table(table_name):
-    try:
-        dynamodb_client.create_table(
-            TableName=table_name,
-            AttributeDefinitions=[{"AttributeName": "LockID", "AttributeType": "S"}],
-            KeySchema=[{"AttributeName": "LockID", "KeyType": "HASH"}],
-            BillingMode="PAY_PER_REQUEST",
-        )
-        print(f"DynamoDB table '{table_name}' created successfully.")
-    except Exception as e:
-        print(f"Failed to create DynamoDB table: {e}")
-
-
-# Main function to create resources and output Terraform backend configuration
-def main():
-    create_s3_bucket(bucket_name)
-    enable_bucket_versioning(bucket_name)
-    create_dynamodb_table(dynamodb_table_name)
-
-    # Output the Terraform backend configuration
-    backend_config = f"""
+    def print_terraform_backend_config(self):
+        backend_config = f"""
 terraform {{
   backend "s3" {{
-    bucket         = "{bucket_name}"
-    dynamodb_table = "{dynamodb_table_name}"
-    key            = "{envname}.tfstate"
-    region         = "{region}"
+    bucket         = "{self.bucket_name}"
+    dynamodb_table = "{self.dynamodb_table_name}"
+    key            = "{self.envname}.tfstate"
+    region         = "{self.region}"
     encrypt        = true
   }}
 }}
 """
-    print("\nAdd the following configuration to your Terraform backend:\n")
-    print(backend_config)
+        print("\nAdd the following configuration to your Terraform backend:\n")
+        print(backend_config)
+
+    def create_resources(self):
+        self.create_s3_bucket()
+        self.enable_bucket_versioning()
+        self.create_dynamodb_table()
+        self.print_terraform_backend_config()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Create resources for Terraform backend.')
+    parser.add_argument('-e', '--envname', type=str, help='Environment name', default=Path.cwd().name)
+    parser.add_argument('-r', '--region', type=str, help='AWS Region', default='us-west-2')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    backend_creator = TerraformBackendCreator(args.envname, args.region)
+    backend_creator.create_resources()
 
 
 if __name__ == "__main__":
